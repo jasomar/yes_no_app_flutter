6e0706a46045043e096e19d7965496499f02dfb0
Jafet Solano <jasomar1998@gmail.com>
2026-02-14 21:44:01 -0600

    text box fill added

--- lib/presentation/screens/chat/chat_screen.dart
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:yes_no_app/presentation/widgets/chat/her_message_bubble.dart';
 import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble_chat.dart';
+import 'package:yes_no_app/presentation/widgets/shared/message_filed_box.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -42,7 +43,7 @@ class _ChatView extends StatelessWidget {
                 },
               ),
             ),
-            Text("data"),
+            const MessageFieldBox(),
           ],
         ),
       ),
